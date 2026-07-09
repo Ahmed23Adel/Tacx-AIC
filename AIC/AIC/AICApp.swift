c2ef5d771fe0f13a9060cff4865f8bb9aed363d5
Ahmed23Adel <ahmed.hussein.1521@gmail.com>
2026-07-09 22:02:01 +0200

    Add basic Xcode project structure

---
+++ AIC/AIC/AICApp.swift
@@ -0,0 +1,17 @@
+//
+//  AICApp.swift
+//  AIC
+//
+//  Created by ahmed on 09/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct AICApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AIC/AICTests/AICTests.swift
@@ -0,0 +1,19 @@
+//
+//  AICTests.swift
+//  AICTests
+//
+//  Created by ahmed on 09/07/2026.
+//
+
+import Testing
+@testable import AIC
+
+struct AICTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
