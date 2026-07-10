35a1770ad03caebd60f777bea1f599400abc07b3
Ahmed23Adel <ahmed.hussein.1521@gmail.com>
2026-07-10 03:36:34 +0200

    Add AppConstants with featured artist

---
+++ AIC/AIC/Constants/AppConstants.swift
@@ -0,0 +1,14 @@
+//
+//  AppConstants.swift
+//  AIC
+//
+//  Created by ahmed on 10/07/2026.
+//
+
+import Foundation
+
+enum AppConstants {
+    enum Artist {
+        static let featured = "Rembrandt van Rijn"
+    }
+}
